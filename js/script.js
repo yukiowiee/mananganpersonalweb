b3f00a8fc363bff795bc692ba46848a690c302a5
yukiowiee <avionna.manangan@mcst.edu.ph>
2026-08-11 00:38:47 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,53 @@
+document.addEventListener("DOMContentLoaded", function () {
 
+    /*
+     * Automatically display the current year
+     */
+
+    const yearElements =
+        document.querySelectorAll("#year");
+
+    yearElements.forEach(function (element) {
+
+        element.textContent =
+            new Date().getFullYear();
+
+    });
+
+
+    /*
+     * Contact Form
+     */
+
+    const contactForm =
+        document.getElementById("contactForm");
+
+
+    if (contactForm) {
+
+        contactForm.addEventListener(
+            "submit",
+            function (event) {
+
+                event.preventDefault();
+
+
+                const formMessage =
+                    document.getElementById(
+                        "formMessage"
+                    );
+
+
+                formMessage.classList.remove(
+                    "d-none"
+                );
+
+
+                contactForm.reset();
+
+            }
+        );
+
+    }
+
+});
